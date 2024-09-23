720719fe8b0074e0428626c0aa246e5297cdf14b
dycgames <dycgames96@gmail.com>
2024-09-23 20:00:05 +0800

    Organized main.c and added helper.h

--- src/main.c
+++ src/main.c
@@ -1,23 +1,58 @@
 #include "raylib.h"
 #include "resource_dir.h"
+#include "helper.h"
+
+
+#define SCREEN_WIDTH 1024
+#define SCREEN_HEIGHT 764
+#define BOX_AREA_SIZE 8192
+
+
+void InitGame(void);
+void UpdateGame(void);
+void RenderGame(void);
+void UnloadGame(void);
+
 
 int main()
 {
 	SetConfigFlags(FLAG_VSYNC_HINT);
-	InitWindow(1024, 768, "Astro-Surge");
-
-	SearchAndSetResourceDir("astro-resources");
+	InitWindow(SCREEN_WIDTH,SCREEN_HEIGHT,"Astro-Surge");
+	InitGame();
 
 	while (!WindowShouldClose())
 	{
+		UpdateGame();
 		BeginDrawing();
 
 		ClearBackground(BLACK);
-		DrawText(TextFormat("%i",GetFPS()), 8, 8, 24, WHITE);
-
+		DrawText(TextFormat("%i",GetFPS()),8,8,20,WHITE);
+		RenderGame();
+		
 		EndDrawing();
 	}
-
-	CloseWindow();
+	
+	UnloadGame();
 	return 0;
+}
+
+
+void InitGame(void)
+{
+	SearchAndSetResourceDir("astro-resources");
+}
+
+void UpdateGame(void)
+{
+	
+}
+
+void RenderGame(void)
+{
+	
+}
+
+void UnloadGame(void)
+{
+	
 }
